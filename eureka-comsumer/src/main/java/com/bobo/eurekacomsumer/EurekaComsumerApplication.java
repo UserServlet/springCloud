6e0089b2f2d0972e0b115e01b04b1672fd1b5c12
wuxiaobo <wuxiaobo@didachuxing.com>
2018-10-19 18:19:06 +0800

    eureka comsumer

---
+++ eureka-comsumer/src/main/java/com/bobo/eurekacomsumer/EurekaComsumerApplication.java
@@ -0,0 +1,25 @@
+package com.bobo.eurekacomsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author wuxiaobo@didachuxing.com
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class EurekaComsumerApplication {
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return  new RestTemplate();
+    }
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaComsumerApplication.class, args);
+    }
+}
---
+++ eureka-comsumer/src/main/java/com/bobo/eurekacomsumer/controller/ConsumerController.java
@@ -0,0 +1,30 @@
+package com.bobo.eurekacomsumer.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * 消费者
+ *
+ * @author wuxiaobo@didachuxing.com
+ * @create 2018-10-16 18:54
+ **/
+@RestController
+public class ConsumerController {
+
+    @Autowired
+    @Qualifier(value = "restTemplate")
+    private RestTemplate restTemplate;
+
+
+    @RequestMapping(value = "/ribbon-consumer", method = RequestMethod. GET)
+    public String helloConsumer () {
+        return restTemplate.getForEntity("http://HELLO-SERVICE/hello",
+                String.class).getBody();
+    }
+}
