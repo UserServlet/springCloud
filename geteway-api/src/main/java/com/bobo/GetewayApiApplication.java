5161444a72f27fb0dc029e7f349278270c627289
wuxiaobo <wuxiaobo@didachuxing.com>
2018-10-28 17:58:09 +0800

    create zuul project

---
+++ geteway-api/src/main/java/com/bobo/GetewayApiApplication.java
@@ -0,0 +1,12 @@
+package com.bobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetewayApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GetewayApiApplication.class, args);
+	}
+}
